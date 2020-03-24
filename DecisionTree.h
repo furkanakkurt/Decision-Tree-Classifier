19c77155e9143182f765f37cebaf68b9e98466bd
Furkan Akkurt <furkanakkurt1905@gmail.com>
2020-03-24 23:38:15 +0300

    zZz

--- DecisionTree.h
+++ DecisionTree.h
@@ -1,10 +1,19 @@
 //#include "DecisionTreeNode.h"
+#include "DecisionTreeNode.h"
 
 class DecisionTree {
 
 private:
+    DecisionTreeNode * root;
 
 public:
+    ~DecisionTree();
+    void DecisionTree::train(const bool**, const int*, const int, const int);
+    void DecisionTree::train(const string, const int, const int);
+    int DecisionTree::predict(const bool*);
+    double DecisionTree::test(const bool**, const int*, const int);
+    double DecisionTree::test(const string, const int);
+    void DecisionTree::print();
     double calculateEntropy( const int * classCounts, const int numClasses);
     double calculateInformationGain(const bool** data, const int* labels,
                                     const int numSamples, const int numFeatures, 
--- DecisionTreeNode.h
+++ DecisionTreeNode.h
@@ -1,7 +1,12 @@
 class DecisionTreeNode {
 
 private:
-
+    DecisionTreeNode * leftPtr;
+    DecisionTreeNode * rightPtr;
+    int feature;
+    bool isLeaf;
 
 public:
+    
+
 };
